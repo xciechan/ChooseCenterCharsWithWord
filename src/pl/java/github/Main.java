9b361810ddca17ff0d4c712530c8e54b9b0d62ed
Maciej Ciechanowski <ciechan92@gmail.com>
2022-05-27 00:38:16 +0200

    add selection method

--- src/pl/java/github/Main.java
+++ src/pl/java/github/Main.java
@@ -5,13 +5,16 @@ package pl.java.github;
  on 2022-05-27
 
  The application selects and displays middle chars with entered word
+ For nonparity of chars - one char
+ For parity of chars - two chars
  */
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(downloadWord());
+        System.out.println("Enter word :)");
+        System.out.println("Middle chars is: " + selectMiddleCharsWithWord(downloadWord()));
 
     }
 
@@ -21,5 +24,18 @@ public class Main {
 
         return  word;
 
+    }
+
+    public static String selectMiddleCharsWithWord(String downloadedWord){
+        String charsWithWord = "";
+
+        if (downloadedWord.length() % 2 == 0){
+            charsWithWord = downloadedWord.substring(downloadedWord.length()/2 - 1, downloadedWord.length()/2 + 1);
+        }
+        else {
+            charsWithWord = downloadedWord.substring(downloadedWord.length()/2, downloadedWord.length()/2 + 1);
+        }
+
+        return charsWithWord;
     }
 }
